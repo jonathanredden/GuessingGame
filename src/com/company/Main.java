b68a9d21b08a5760a3109a2deb902b580390d76f
jonathanredden <jonathancredden@gmail.com>
2018-01-30 20:32:34 -0500

    Creating projects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,59 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        //1. Allow user input of a number (guess)
-        //2. Tell the user if their guess is to high or too low
-        //3 Allow for 5 chances, if the nu
-        //4
-        //5
+        /*
+        1. Allow user input of a number (guess)
+        2. Tell the user if their guess is to high or too low.
+        3. Allow for 5 chances, if the number isn't guessed tell them what it is.
+        4. if the number is guessed, congratulate them.
+        5. Allow the user to play again.
+        */
+        int higher = 100;
+        int lower = 0;
+        int random = (int) (Math.random() * 50 + 1);
+        int attempts = 0;
+        int attemptLimit = 5;
+
+        System.out.println("Guess a number between 0 and 100: ");
+
+        Scanner scan = new Scanner(System.in);
+        int userGuess = scan.nextInt();
+
+        System.out.println(userGuess);
+
+        do {
+            if (userGuess > random) {
+                System.out.println("Guessed too high!");
+            } else if (userGuess < random) {
+                System.out.println("Guessed too low!");
+            } else if (userGuess == random); {
+                System.out.println("you won!");
+            }
+        } while (attempts == 0);
 
 
-        Scanner input = new Scanner(System.in);
 
-        System.out.println(x:"Enter your guess: ");
-        int userGuess = input.nextInt();
 
+
+
+//        Scanner input = new Scanner(System.in);
+//
+//        System.out.println("Enter your guess: ");
+//        int userGuess = input.nextInt();
+//
+//        int generatedNumber = (int) Math.ceil(Math.random() * 100);
+//
+//        if (userGuess > generatedNumber) {
+//            System.out.println("Your number is too high!");
+//        } else if (userGuess < generatedNumber){
+//            System.out.println("Your number is too low!");
+//        }
+//
+//
     }
 }
